71e119fdeb059cfe288925d1f6197990ef817438
Gavin Le <quocle@mail.usf.edu>
2020-02-23 11:42:43 -0500

    test function

--- MEMORY TRACES/memsim.c
+++ MEMORY TRACES/memsim.c
@@ -1,35 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/queue.h>
 #include <limits.h>
+#include <stdbool.h>
 
-void lru (char* fileName, int nframes, char* mode) {
+int TRACESIZE = 100000;
 
-}
+typedef struct{
+    char vpn;
+    char d;
+} Trace;
 
-void fifo (char* fileName, int nframes, char* mode) {
-
-}
-
-void vms (char* fileName, int nframes, char* mode) {
-
-}
-
-void vms (char* fileName, int nframes, char* mode) {
+void lru (Trace* traceFile, int nframes, char* mode) {
 
 }
 
+void fifo (Trace* traceFile, int nframes, char* mode) {
 
+}
+
+void vms (Trace* traceFile, int nframes, char* mode) {   
+    printf("PageNumber is %d", 20);
+    int i;
+    for(i = 0; i < TRACESIZE; i++)
+    {
+        printf(traceFile[i].vpn + " " + traceFile[i].d + '\n');
+    }
+}
+
+void rmd (Trace* traceFile, int nframes, char* mode) {
+
+}
+
+void readTraceFile (FILE* file, Trace* traceFile){
+    char address;
+    char rw;
+    int i;
+
+    for(i = 0; fscanf(file, "%s %c", &address, &rw) != EOF; i++)
+    {
+        traceFile[i].vpn = address;
+        traceFile[i].d = rw;
+    }
+    fclose(file);
+}
 
 int main(int argc, char *argv[]) {
-    char *trace_file = argv[1];
+    char *fileName = argv[1];
+    Trace traceFile[TRACESIZE];
     int nframes = atoi(argv[2]);
     char *algo = argv[3];
     char *mode = argv[4];
 
 	FILE * file;
 	file = fopen(argv[1], "r");
+    if(!file) {
+        fprintf(stderr, "ERROR: Can't open %s\n", fileName);
+        exit(1); 
+    }
+    else
 
     if (strcmp(algo, "lru") == 0) 
     {
@@ -37,6 +66,7 @@ int main(int argc, char *argv[]) {
     }
     else if (strcmp(algo, "rmd") == 0) {
         printf("--- RMD: Randomly Selects to Replace --\n");
+        lru(traceFile, nframes, mode);
     }
     else if (strcmp(algo, "fifo") == 0)
     {
@@ -45,6 +75,7 @@ int main(int argc, char *argv[]) {
     else if (strcmp(algo, "vms") == 0)
     {
         printf("--- VMS --\n");
+        vms(traceFile, nframes, mode);
     }
     else
     {
