c2c372f15b385c01e6365bd091ee446640eff4fd
Aungkin <60588737+Aungkin@users.noreply.github.com>
2020-02-27 12:08:32 -0500

    Update memsim.c

--- MEMORY TRACES/memsim.c
+++ MEMORY TRACES/memsim.c
@@ -1,15 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/queue.h>
 #include <limits.h>
+#include <stdbool.h>
+
 
 void lru (char* fileName, int nframes, char* mode) {
 
+    unsigned addr;
+    char rw;
+    FILE *fp;
+    unsigned LRU[nframes];
+
+    //open file. If open failed, exit.
+    fp = fopen(fileName, "r");
+    if(fp ==NULL){printf("ERROR Opening file\n");   exit(1);}
+
+    while(fscanf(fileName,"%x %c",&addr,&rw) !=EOF)
+    {
+
+    }
 }
 
+struct trace   
+{
+    unsigned vpn;   //page number
+    char d;        //Read/Write
+    int dirt;          //dirty or not
+};
+
 void fifo (char* fileName, int nframes, char* mode) {
-printf("hi");
+
+    unsigned addr;
+    char rw;
+    FILE *fp;
+    struct trace FIFO[nframes];
+    int k =0;   //keep track of oldest page 
+
+    //open file. If open failed, exit.
+    fp = fopen(fileName, "r");
+    if(fp ==NULL){printf("ERROR Opening file\n");   exit(1);}
+
+    //while not end of file, scan address and rw.
+    while(fscanf(fileName,"%x %c",&addr,&rw) !=EOF)
+    {
+        struct trace temp;
+        addr = addr >>12;
+        temp.vpn = addr;
+        temp.d = rw; 
+
+        if(k==nframes){k=0;}    //if page is last, oldest page is at index 0
+
+        if(mode == "debug") {printf("%x\t%c\n", addr, rw); }  //debug print 
+
+        // for(int i =0; i < nframes; i++) //check address in frame
+        // {
+        //     if(FIFO[i].vpn == NULL)   //if frame has empty slot add to queue
+        //     {
+        //         FIFO[i] =temp;
+        //     }
+        //     else    //if no empty slot check if address is in
+        //     {
+        //         if(FIFO[i].vpn == addr) //if address exist in frame
+        //         {
+
+        //         }
+        //         else    //else. if address not in frame
+        //         {
+                    
+        //         }
+        //     }            
+        // }
+        
+        if(FIFO[k].vpn == NULL)
+        {FIFO[k] = temp;}
+        
+
+    }
+
 }
 
 void vms (char* fileName, int nframes, char* mode) {
