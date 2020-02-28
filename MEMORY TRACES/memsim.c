57bf7fdd4e192485652ce6a5cbbf1fa51d2313cf
Gavin Le <quocle@mail.usf.edu>
2020-02-27 21:31:41 -0500

    Update with rmd and fifo

--- MEMORY TRACES/memsim.c
+++ MEMORY TRACES/memsim.c
@@ -2,48 +2,221 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include <time.h>
 #include <stdbool.h>
 
-typedef struct{
-    unsigned vpn;
-    char d;
-} Trace;
+int i;
+int nEvent = 0, nRead = 0, nWrite = 0, nHit = 0, nMiss = 0;
+int newPageLocation;
+int foundLocation;
+int olderPageLocation = 0;
 
-void lru (FILE* file, int nframes, char* mode) {
+struct Trace{
+    unsigned vpn;
+    bool isDirty;
+};
+
+void lru (FILE* file, int nframes, bool isDebug) {
     
 }
 
-void fifo (FILE* file, int nframes, char* mode) {
+void vms (FILE* file, int nframes, bool isDebug) {  
 
 }
 
-void vms (FILE* file, int nframes, char* mode) {  
-
+/***************************************************************************************************************/
+bool rmdIsExist (struct Trace* table, int nFrames, unsigned addr, bool isDebug){
+    for (i = 0; i < nFrames; i++){
+        if (table[i].vpn == addr){
+            foundLocation = i;
+            return true;
+        }
+    }
+    return false;
 }
 
-void rmd (FILE* file, int nframes, char* mode) {
-    Trace page_table[nframes];
+bool isFull (struct Trace* table, int nFrames, bool isDebug){
+    for (i = 0; i < nFrames; i++)
+    {
+        if (table[i].vpn == -1){
+            newPageLocation = i;
+            return false;
+        }
+    }
+    return true;
+}
+
+void rmd (FILE* file, int nframes, bool isDebug) {
+    struct Trace page_table[nframes];
     unsigned addr;
     char d;
-    int i = 0;
-
-    for (i = 0; i < nframes; i++)
-    {
-        fscanf(file, "%x %c", &addr, &d);
-        page_table[i].vpn = addr>>12;
-        page_table[i].d = d;
-        printf("%x\n", page_table[i].vpn);
+    
+    for (i = 0; i < nframes; i++) {
+        page_table[i].vpn = -1;
     }
+
+    while (fscanf(file, "%x %c", &addr, &d) != EOF){
+        int nRandonNumber;
+        nEvent++;
+        struct Trace newPage;
+        newPage.vpn = addr>>12;
+        bool full = isFull(page_table, nframes, isDebug);
+        bool isExist = rmdIsExist(page_table, nframes, newPage.vpn, isDebug);
+
+        
+        if (d == 'w' || d == 'W'){ newPage.isDirty = true; }
+        else { newPage.isDirty = false; }
+        if (isDebug){
+            printf ("\nNew Address is: %x | %c\n", addr, d);
+            printf ("Page number is: %x\n", newPage.vpn);
+        }
+
+        if (full) {
+            if (isDebug){printf("Table is full\n");}
+            if (!isExist){
+                nRandonNumber = rand()%nframes;
+                if (page_table[nRandonNumber].isDirty){
+                    nWrite++;
+                }
+                page_table[nRandonNumber] = newPage;
+                if (isDebug){
+                    printf("Page %x is not exist in table\n", newPage.vpn);
+                    printf("Replace page %x to random location %d\n", newPage.vpn, nRandonNumber);
+                }
+                nRead++;
+            }
+            else {
+                if (page_table[foundLocation].isDirty){
+                    if (!newPage.isDirty){
+                        page_table[foundLocation] = newPage;
+                        nWrite++;
+                        nRead++;
+                    }
+                }
+                if (isDebug){printf("Page %x is exist at location %d\n", newPage.vpn, foundLocation);}
+            }
+        }
+        else {
+            if (isDebug){printf("Table is not full\n");}
+            if (!isExist) { 
+                page_table[newPageLocation] = newPage;
+                if (isDebug) {
+                    printf("Page %x is not exist in table\n", newPage.vpn);
+                    printf("Add page %x to location %d", newPage.vpn, newPageLocation);
+                }
+                nRead++;
+            }
+            else {
+                if (page_table[foundLocation].isDirty){
+                    if (!newPage.isDirty){
+                        page_table[foundLocation] = newPage;
+                    }
+                }
+                if (isDebug){printf("Page %x is exist at location %d\n", newPage.vpn, foundLocation);}
+            }
+        }
+    }
+    printf("\nTotal memory frames: %d\n", nframes);
+    printf("Events in trace: %d\n", nEvent);
+    printf("Total disk reads: %d\n", nRead);
+    printf("Total disk writes: %d\n", nWrite);
 }
+/***************************************************************************************************************/
+
+void fifo (FILE* file, int nframes, bool isDebug) {
+    struct Trace page_table[nframes];
+    unsigned addr;
+    char d;
+    
+    for (i = 0; i < nframes; i++) {
+        page_table[i].vpn = -1;
+    }
+
+    while (fscanf(file, "%x %c", &addr, &d) != EOF){
+        nEvent++;
+        struct Trace newPage;
+        newPage.vpn = addr>>12;
+        bool full = isFull(page_table, nframes, isDebug);
+        bool isExist = rmdIsExist(page_table, nframes, newPage.vpn, isDebug);
+
+        
+        if (d == 'w' || d == 'W'){ newPage.isDirty = true; }
+        else { newPage.isDirty = false; }
+        if (isDebug){
+            printf ("\nNew Address is: %x | %c\n", addr, d);
+            printf ("Page number is: %x\n", newPage.vpn);
+        }
+
+        if (full) {
+            if (isDebug){printf("Table is full\n");}
+            if (!isExist){
+                if (page_table[olderPageLocation].isDirty){
+                    nWrite++;
+                }
+                page_table[olderPageLocation] = newPage;
+                olderPageLocation++;
+                if (isDebug){
+                    printf("Page %x is not exist in table\n", newPage.vpn);
+                    printf("Replace page %x to random location %d\n", newPage.vpn, olderPageLocation);
+                }
+                nRead++;
+            }
+            else {
+                if (page_table[foundLocation].isDirty){
+                    if (!newPage.isDirty){
+                        page_table[foundLocation] = newPage;
+                    }
+                }
+                if (isDebug){printf("Page %x is exist at location %d\n", newPage.vpn, foundLocation);}
+            }
+        }
+        else {
+            if (isDebug){printf("Table is not full\n");}
+            if (!isExist) { 
+                page_table[newPageLocation] = newPage;
+                if (isDebug) {
+                    printf("Page %x is not exist in table\n", newPage.vpn);
+                    printf("Add page %x to location %d", newPage.vpn, newPageLocation);
+                }
+                nRead++;
+            }
+            else {
+                if (page_table[foundLocation].isDirty){
+                    if (!newPage.isDirty){
+                        page_table[foundLocation] = newPage;
+                    }
+                }
+                if (isDebug){printf("Page %x is exist at location %d\n", newPage.vpn, foundLocation);}
+            }
+        }
+        if (olderPageLocation == nframes){olderPageLocation = 0;}
+    }
+    printf("\nTotal memory frames: %d\n", nframes);
+    printf("Events in trace: %d\n", nEvent);
+    printf("Total disk reads: %d\n", nRead);
+    printf("Total disk writes: %d\n", nWrite);
+}
+
+/***************************************************************************************************************/
 
 int main(int argc, char *argv[]) {
     char *fileName = argv[1];
     int nframes = atoi(argv[2]);
     char *algo = argv[3];
     char *mode = argv[4];
-    unsigned addr;
-    char d;
-    int i = 0;
+    bool isDebug;
+
+    if (strcmp(mode, "debug") == 0){
+        isDebug = true;
+    }
+    else if (strcmp(mode, "quiet") == 0)
+    {
+        isDebug = false;
+    }
+    else
+    {
+        printf("Invalid Mode");
+    }
 
 	FILE *file;
 	file = fopen(argv[1], "r");
@@ -57,17 +230,18 @@ int main(int argc, char *argv[]) {
         printf("--- LRU: Last Recently Used --\n");
     }
     else if (strcmp(algo, "rmd") == 0) {
-        printf("--- RMD: Randomly Selects to Replace --\n");
-        rmd(file, nframes, mode);
+        printf("RMD '%s' with %d frames in %s mode\n", fileName, nframes, mode);
+        rmd(file, nframes, isDebug);
     }
     else if (strcmp(algo, "fifo") == 0)
     {
         printf("--- FIFO: First in First Out --\n");
+        fifo(file, nframes, isDebug);
     }
     else if (strcmp(algo, "vms") == 0)
     {
-        printf("--- VMS --\n");
-        vms(file, nframes, mode);
+        printf("VMS '%s' with %d frames in %s mode\n", fileName, nframes, mode);
+        vms(file, nframes, isDebug);
     }
     else
     {
