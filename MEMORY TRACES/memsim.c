b755a390db38679eff99af9b416b4b69885adefb
Gavin Le <quocle@mail.usf.edu>
2020-02-28 20:38:48 -0500

    Update memsim.c

--- MEMORY TRACES/memsim.c
+++ MEMORY TRACES/memsim.c
@@ -152,55 +152,6 @@ bool addExist(struct Trace* arr, int n, unsigned ad)
 
 void vms (FILE* file, int nframes, bool isDebug) {  
 
-    unsigned addr;
-    char rw;
-    FILE *fp;
-    struct trace FIFO[nframes];
-    int s, k =0;   //keep track of oldest page 
-    
-    //open file. If open failed, exit.
-    fp = fopen(file, "r");
-    if(fp ==NULL){printf("ERROR Opening file\n");   exit(1);}
-
-    //while not end of file, scan address and rw.
-    while(fscanf(file,"%x %c",&addr,&rw) !=EOF)
-    {
-        struct trace temp;
-        addr = addr >>12;
-        temp.vpn = addr;
-        temp.d = rw; 
-        s=0;
-
-        if(k==nframes){k=0;}    //if page is last, oldest page is at index 0
-
-        if(isDebug) {printf("%x\t%c\n", addr, rw); }  //debug print 
-
-        //check if address exist in frame
-        for(int i =0;i <nframes; i++)
-        {
-            if(FIFO[i].vpn == addr) //if address exist in frame
-                {
-                    //do something with that address in frame i
-                    s++;
-                    k--;   
-                    }
-        }
-        k++;
-            
-        if((k <= nframes) && (s==0))
-        {
-        }
-        else if(s==0)
-        {
-        }
-
-        // //check if oldest slot is empty, insert page, and 
-        // if(FIFO[k].vpn == NULL)
-        // {FIFO[k] = temp; k++;}
-        // else
-        // {
-    }
-
 }
 
 /***************************************************************************************************************/
